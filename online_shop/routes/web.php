1189415573d5acacf55ebce40d328b5a03859c60
dathaohus <bombomqt98@gmail.com>
2019-11-13 05:14:15 +0700

    xử lý đăng nhập cho admin

--- online_shop/routes/web.php
+++ online_shop/routes/web.php
@@ -14,3 +14,67 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+/**
+ * Route cho administrator
+ */
+Route::prefix('admin')->group(function (){
+    /*
+     * gom nhóm các route trong phần admin bằng phương thức group
+     * */
+    /**
+     * URL : online_shop.com/admin/
+     * Route mặc định của admin
+     */
+    Route::get('/','AdminController@index')->name('admin.dashboard');
+    /**
+     * URL : online_shop.com/admin/dashboard
+     * Route đăng nhập thành công
+     */
+
+    Route::get('/dashboard','AdminController@index')->name('admin.dashboard');
+
+    /**
+     * URL : online_shop.com/admin/register
+     * Route trả về để đăng ký tài khoản admin
+     */
+    Route::get('register','AdminController@create')->name('admin.register');
+
+    /**
+     * URL : online_shop.com/admin/register
+     * Phương thức là POST
+     * Route dùng để đăng ký 1 admin từ form POST
+     */
+    Route::post('register','AdminController@store')->name('admin.register.store');
+
+
+    /**
+     * Method get
+     * URL: online_shop/admin/login
+     * Route trả về view đăng nhập admin
+     */
+    Route::get('login','Auth\Admin\LoginController@login')->name('admin.auth.login');
+
+    /**
+     * URL: online_shop/admin/login
+     * Route xử lý quá trình đăng nhập admin
+     * Method post
+     */
+    Route::post('login','Auth\Admin\LoginController@loginAdmin')->name('admin.auth.loginAdmin');
+
+    /**
+     * method post
+     * URL online_shop/admin/logout
+     * Route dùng để đăng xuất
+     */
+    Route::post('logout','Auth\Admin\LoginController@logout')->name('admin.auth.logout');
+
+    /**
+     * Route cho các nhà cung cấp sản phẩm(seller)
+     */
+
+
+});
