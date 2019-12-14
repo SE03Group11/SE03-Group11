5a25c345e07991e5a6871cffa7a486287a53bfd5
thuytt-hus <thuytt.hus@gmail.com>
2019-12-14 17:08:56 +0700

    Tích hợp laravel file-manager để quản lý các file trong dự án

--- shop-online/resources/views/admin/content/media/index.blade.php
+++ shop-online/resources/views/admin/content/media/index.blade.php
@@ -4,4 +4,7 @@
 @endsection
 @section('content')
     <h1> Quản trị media</h1>
+    <div style="margin-top: 30px">
+    <iframe src="http://localhost/SE03-Group11/shop-online/public/laravel-filemanager" style="width: 100%; height: 500px; overflow: hidden; border: none;"></iframe>
+    </div>
 @endsection
