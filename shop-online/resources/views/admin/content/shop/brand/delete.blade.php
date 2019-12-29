f73e9a3d373d6d546ea5f14f9a81776c6a5f9aff
dathaohus <bombomqt98@gmail.com>
2019-12-30 04:45:45 +0700

    Xây dựng tính năng thêm sửa xóa cho phần shop/brand trong admin

--- shop-online/resources/views/admin/content/shop/brand/delete.blade.php
+++ shop-online/resources/views/admin/content/shop/brand/delete.blade.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 11/17/2019
- * Time: 02:37 PM
- */
+@extends('admin.layouts.glance')
+@section('title')
+    Danh mục nội dung
+@endsection
+@section('content')
+    <h1> Xóa nhãn hàng {{ $brand->id . ' : ' .$brand->name }}</h1>
+
+    <div class="row">
+        <div class="form-three widget-shadow">
+            <form name="page" action="{{ url('admin/shop/brand/'.$brand->id.'/delete') }}" method="post" class="form-horizontal">
+                @csrf
+
+                <div class="col-sm-offset-2">
+                    <button type="submit" class="btn btn-danger">Xóa</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@endsection
