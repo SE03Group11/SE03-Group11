cca736f36a7de508b252ca3e46da020ea152e1cd
thuytt-hus <thuytt.hus@gmail.com>
2020-01-04 02:30:44 +0700

    Xây dựng nội dung hiển thị phần content category, content tag

--- shop-online/app/Http/Controllers/Admin/ContentTagController.php
+++ shop-online/app/Http/Controllers/Admin/ContentTagController.php
@@ -6,7 +6,6 @@ use App\Model\Admin\ContentTagModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-
 class ContentTagController extends Controller
 {
     //
@@ -15,11 +14,25 @@ class ContentTagController extends Controller
      * Hàm này nó luôn được chạy trước các hàm khác trong class
      * AdminController constructor.
      */
-
     public function __construct()
     {
         $this->middleware('auth:admin');
     }
+
+    public function slugify($str) {
+        $str = trim(mb_strtolower($str));
+        $str = preg_replace('/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/', 'a', $str);
+        $str = preg_replace('/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/', 'e', $str);
+        $str = preg_replace('/(ì|í|ị|ỉ|ĩ)/', 'i', $str);
+        $str = preg_replace('/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/', 'o', $str);
+        $str = preg_replace('/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/', 'u', $str);
+        $str = preg_replace('/(ỳ|ý|ỵ|ỷ|ỹ)/', 'y', $str);
+        $str = preg_replace('/(đ)/', 'd', $str);
+        $str = preg_replace('/[^a-z0-9-\s]/', '', $str);
+        $str = preg_replace('/([\s]+)/', '-', $str);
+        return $str;
+    }
+
     public function index() {
         $items = DB::table('content_tags')->paginate(10);
 
@@ -38,7 +51,6 @@ class ContentTagController extends Controller
          */
         $data = array();
 
-
         return view('admin.content.content.tag.submit', $data);
     }
 
@@ -50,7 +62,6 @@ class ContentTagController extends Controller
 
         $item = ContentTagModel::find($id);
         $data['tag'] = $item;
-
 
         return view('admin.content.content.tag.edit', $data);
     }
@@ -71,7 +82,6 @@ class ContentTagController extends Controller
 
         $validatedData = $request->validate([
             'name' => 'required|max:255',
-            'slug' => 'required',
             'images' => 'required',
             'intro' => 'required',
         ]);
@@ -81,7 +91,7 @@ class ContentTagController extends Controller
         $item = new ContentTagModel();
 
         $item->name = $input['name'];
-        $item->slug = $input['slug'];
+        $item->slug = $input['slug'] ? $this->slugify($input['slug']) : $this->slugify($input['name']);
         $item->images = $input['images'];
         $item->author_id = isset($input['author_id']) ? $input['author_id'] : 0;
         $item->view = isset($input['view']) ? $input['view'] : 0;
@@ -92,12 +102,10 @@ class ContentTagController extends Controller
         return redirect('/admin/content/tag');
     }
 
-
     public function update(Request $request, $id) {
 
         $validatedData = $request->validate([
             'name' => 'required|max:255',
-            'slug' => 'required',
             'images' => 'required',
             'intro' => 'required',
         ]);
@@ -107,7 +115,7 @@ class ContentTagController extends Controller
         $item = ContentTagModel::find($id);
 
         $item->name = $input['name'];
-        $item->slug = $input['slug'];
+        $item->slug = $input['slug'] ? $this->slugify($input['slug']) : $this->slugify($input['name']);
         $item->images = $input['images'];
         $item->intro = $input['intro'];
         $item->author_id = isset($input['author_id']) ? $input['author_id'] : 0;
